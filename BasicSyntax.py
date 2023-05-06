6a78a4164e1b459529de7195c7f8d1f217328f2f
clarenceonya <clarenceechia@gmail.com>
2023-05-05 23:06:08 -0400

    Exciting work

--- BasicSyntax.py
+++ BasicSyntax.py
@@ -8,6 +8,3 @@ print ("my name is :", name, "i am :",age,"years old")
 a=5
 b=a+10
 print(b)
-work=input("what is your job title :")
-country=input("what is your country :")
-print ("my job title is :", work, "my country is :", country)
